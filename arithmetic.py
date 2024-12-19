1b4e509e66f8aca38b2071a6036584f999ef6872
Pranesh Kaushik <pranesh@mobitrail.local>
2024-12-19 16:36:55 +0530

    first new commit

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+
+
+
+
+def add(a,b):
+    c = a+b
+    return c 
+
+
+print(add(5,10))
+
+
+def subtract(a,b):
+    return a-b
+
+print(subtract(10,5))
+
+
+def multiply(a,b):
+    return a*b
+print(multiply(5,5))
+
+
+def division(a,b):
+    return a/b
+
+print(division(15,5))
